9fd7f77d447ab6cbff03cb9484c205676234e04e
Rames615 <ramostrick@gmx.com>
2025-03-18 15:25:51 +0100

    commenting the code js in app

--- assets/app.js
+++ assets/app.js
@@ -1,48 +1,32 @@
 
-
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const parallaxElements = document.querySelectorAll(".parallax");
-  
-//     function updateParallax() {
-//       let scrollY = window.scrollY;
-  
-//       parallaxElements.forEach((element, index) => {
-//         let speed = 0.6 + index * 0.001; 
-//         let yPos = -(scrollY * speed);
-//         element.style.transform = `translateY(${yPos}px)`;
-//       });
-  
-//       window.addEventListener('scroll', updateParallax);
-//     }
-  
-//     updateParallax(); 
-//   });
-  
-
-  //////////////////////////////////////////////////////////////////////
+// on attend le chargement du DOM
 
   document.addEventListener('DOMContentLoaded', function() {
-    // Sélectionner tous les éléments avec l'effet parallax
+    // Sélectionner et stocké tous les éléments avec l'effet parallax
     const parallaxElements = document.querySelectorAll('.parallax');
 
     const hero = document.querySelector('.hero');
     function updateParallax() {
+        // je recupere la position de la page
         const scrollTop = window.pageYOffset;
         if (hero) {
             hero.style.backgroundPositionY = scrollTop * 0.001 + 'px';
         }
-
+        // je boucle sur les elements parallax
         parallaxElements.forEach(element => {
-        
+        // je recupere le parent de l'element
             const section = element.parentElement;
+            // je recupere la position du parent
 
             const sectionTop = section.offsetTop;
             const sectionHeight = section.offsetHeight;
 
-            if (scrollTop + window.innerHeight > sectionTop && scrollTop < sectionTop + sectionHeight) {
+            // je verifie si l'element est visible
+            // si la position de la page + la hauteur de la fenetre est superieur a la position du parent et la position de la page est inferieur a la position du parent + la hauteur du parent
 
-                const yPos = (scrollTop - sectionTop) * 1.1;
+            if (scrollTop + window.innerHeight > sectionTop && scrollTop < sectionTop + sectionHeight) {
+            // je stocke la position de la page - la position du parent
+                const yPos = (scrollTop - sectionTop) * 0.4;
                 
                 element.style.transform = `translateY(${yPos}px)`;
             }
@@ -51,5 +35,7 @@
 
     // Mettre à jour l'effet parallax lors du scroll
     window.addEventListener('scroll', updateParallax);
-    updateParallax();
+    
 });
+// initialiser l'effet parallax
+updateParallax();
